3619aaa58b0f13c858c4bfe9ddd5b975144bd6d1
hajun0810 <dhgkwns010810@naver.com>
2024-06-18 09:05:36 +0900

    json

--- javascript/user_management/user_management.js
+++ javascript/user_management/user_management.js
@@ -1,9 +1,13 @@
-const names = ["이름1","이름2","이름3"];
-console.log(names.join("")); //innerHTML은 기본값이 join() 이다.
+// inputMode = 1 > 추가
+// inputMode = 2 > 수정
+let inputMode = 1;
 
 let userList = [];
+loadUserList();
 
 let emptyUser = {
+    id: 0,
+    name: "",
     username: "",
     password: ""
 };
@@ -12,46 +16,79 @@ let user = {
     ...emptyUser
 }
 
-function renderTable(){
+function renderTable() {
     const userTableBody = document.querySelector(".user-table-body");
-    userTableBody.innerHTML = userList.map(({username,password},index) => {
+    userTableBody.innerHTML = userList.map(({id, name, username, password}, index) => {
         return `
             <tr>
-                <td>${index+1}</td>
+                <th><input type="checkbox" onchange="handleUserCheck(event)"></th>
+                <td>${index + 1}</td>
+                <td>${id}</td>
+                <td>${name}</td>
                 <td>${username}</td>
                 <td>${password}</td>
+                <th><button onclick="deleteUser(event)" value="${id}">삭제</button></th>
             </tr>
         `;
-    }).join(""); /* = 배열을 문자열로 변환 시키는 코드  */
+    }).join("");
 }
 
-function handleUserInputKeyDown(e){
+function handleUserInputKeyDown(e) {
     user = {
         ...user,
         [e.target.name]: e.target.value
-     }
+    }
 
-     console.log(user);
+    // user[e.target.name] = e.target.value;
 
-    if(e.keyCode === 13){
-        const passwordInput = document.querySelector(".password-input");
+    if(e.keyCode === 13) {
+        const nameInput = document.querySelector(".name-input");
         const usernameInput = document.querySelector(".username-input");
-        if(e.target.name === "username"){
-            passwordInput.focus();
-            
-        }
-         if(e.target.name === "password"){
-            //user의 객체주소가 같을 경우에, 데이터를 덮어씌운다. -> 같은 값만 나온다
-            userList = [...userList,{...user}]; 
+        const passwordInput = document.querySelector(".password-input");
 
+        if(e.target.name === "name") {
+            usernameInput.focus();
+        }
+        if(e.target.name === "username") {
+            passwordInput.focus();
+        }
+        if(e.target.name === "password") {
+            userList = [ ...userList, { ...user, id: getNewId() } ];
+
+            saveUserList();
             renderTable();
 
+            nameInput.value = emptyUser.name;
             usernameInput.value = emptyUser.username;
             passwordInput.value = emptyUser.password;
 
-            usernameInput.focus();
+            nameInput.focus();
         }
-    }  
+    }
+}
 
-    console.log(e.target.name);
+function saveUserList() {
+    localStorage.setItem("userList", JSON.stringify(userList));
+}
+
+function loadUserList() {
+    const lsUserList = localStorage.getItem("userList");
+    userList = !lsUserList ? [] : JSON.parse(lsUserList);
+    renderTable();
+}
+
+function deleteUser(e) {
+    userList = userList.filter(({id}) => id !== parseInt(e.target.value));
+    saveUserList();
+    renderTable();
+}
+
+function getNewId() {
+    const userIds = userList.map(user => user.id);
+    const maxUserId = userIds.length === 0 ? 20240000 : Math.max.apply(null, userIds);
+    return maxUserId + 1;
+}
+
+function handleUserCheck(e) {
+    
 }
